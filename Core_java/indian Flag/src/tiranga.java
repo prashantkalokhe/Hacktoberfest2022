323f020e46549b525b63083b725b61ab80f1d64e
munishyadav08 <115063305+munishyadav08@users.noreply.github.com>
2022-10-15 23:21:55 -0700

    Update tiranga.java
    added java tiranga program

--- Core_java/indian Flag/src/tiranga.java
+++ Core_java/indian Flag/src/tiranga.java
@@ -29,3 +29,42 @@ public class tiranga {
 	}
 
 }
+
+package AWTP;
+
+import java.applet.*;
+import java.awt.*;
+public class Flag extends Applet
+{
+    @Override
+public void paint(Graphics g)
+{
+g.fillOval(60,450,120,50);
+g.fillRect(110,60,10,400);
+g.setColor(Color.red);
+g.fillRect(120,80,150,30);
+g.setColor(Color.white);
+g.fillRect(120,110,150,30);
+g.setColor(Color.green);
+g.fillRect(120,140,150,30);
+g.setColor(Color.black);
+g.drawRect(120,80,150,90);
+g.setColor(Color.blue);
+
+g.drawOval(180,110,30,30);
+int t=0;
+int x=195,y=125;
+double x1,y1;
+double r=15;
+double d;
+for(int i=1;i<=24;i++)
+{
+d=(double)t*3.14/180.0;
+x1=x+(double)r*Math.cos(d);
+y1=y+(double)r*Math.sin(d);
+g.drawLine(x,y,(int)x1,(int)y1);
+t+=360/24;
+}
+}
+}
+
