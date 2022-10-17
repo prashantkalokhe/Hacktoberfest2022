8082ed3018c89c4f18784fad67139cc7374e3aa6
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2022-10-17 17:40:22 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,4 +1,18 @@
+/*
+step 1: start
 
+step 2: declare array and left, right, mid variable
+
+step 3: perform merge function.
+    if left > right
+        return
+    mid= (left+right)/2
+    mergesort(array, left, mid)
+    mergesort(array, mid+1, right)
+    merge(array, left, mid, right)
+
+step 4: Stop
+*/
 
 public class MergeSort {
 
