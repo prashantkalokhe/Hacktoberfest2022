fb67cb222d86230c5166dc499a1b39c0835969b9
mehra2001 <86433688+mehra2001@users.noreply.github.com>
2022-10-18 15:56:11 +0530

    Create singleNumber.java

---
+++ Java_DSA/singleNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+}
